7a0026486ddd8b5a7b76e8a74782e2ca9a00b796
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-06-29 22:00:52 -0300

    Used mapState() to map the state of the store, you added data about nurses, first responders, doctors, cars, phones, and kits, and listed this data on their respective presentation routes.

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>
-        Item
+    <div class="row">
+        <div class="col-1" v-if="dados.id">{{dados.id}}</div>
+        <div class="col" v-if="dados.nome">{{dados.nome}}</div>
+        <div class="col" v-if="dados.placa">{{dados.placa}}</div>
+        <div class="col" v-if="dados.telefone">{{dados.telefone}}</div>
+        <div class="col" v-if="dados.kit">{{dados.kit}}</div>
+        <div class="col-2" v-if="dados.escala">{{dados.escala}}</div>
+        <div class="col-2" v-if="dados.turno">{{dados.turno}}</div>
+        <div class="col-1">
+            <i class="bi-check2-square" style="cursor: pointer"></i>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Item'
+    name: 'Item',
+    props: {
+        dados: Object
+    }
 }
 </script>
